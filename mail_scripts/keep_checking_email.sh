2c6a51020ac0ad8790aa3562feca6b7b02a9ba75
Jose Abel Castellanos Joo <jabelcastellanosjoo@unm.edu>
2022-12-19 14:30:51 -0700

    Update keep checking email script.

--- mail_scripts/keep_checking_email.sh
+++ mail_scripts/keep_checking_email.sh
@@ -3,13 +3,13 @@
 # To enable mbsync write 'yes' on the file $ENABLE_MBSYNC
 # To disable mbsync write anything but 'yes' on the file $ENABLE_MBSYNC
 
-ENABLE_MBSYNC=/home/jose/.enable_mbsync
-FILE=/home/jose/.unread_email
-DIR=/home/jose/Mail/unm/Inbox/new
+ENABLE_MBSYNC=/Users/typesarespaces/.enable_mbsync
+FILE=/Users/typesarespaces/.unread_email
+DIR=/Users/typesarespaces/Mail/unm/Inbox/new
 
 while [ 1 ]; do
   # python ~/.local/scripts/UnseenMail/UnseenMail.py
-  if [ "$(cat .enable_mbsync | xargs)" = "yes" ]; then mbsync -a; fi
+  if [ "$(cat $ENABLE_MBSYNC | xargs)" = "yes" ]; then mbsync -a; fi
   echo ' '> $FILE
   ls $DIR | wc -l | xargs >> $FILE
   sleep 15;
