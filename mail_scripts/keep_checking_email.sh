83ae4515379f92fa825a3b6f8b4ed5c7211d4430
jose <jabelcastellanosjoo@unm.edu>
2022-12-29 15:29:25 -0700

    Replace /home/jose by either $HOME or ~

--- mail_scripts/keep_checking_email.sh
+++ mail_scripts/keep_checking_email.sh
@@ -3,12 +3,11 @@
 # To enable mbsync write 'yes' on the file $ENABLE_MBSYNC
 # To disable mbsync write anything but 'yes' on the file $ENABLE_MBSYNC
 
-ENABLE_MBSYNC=/home/jose/.enable_mbsync
-FILE=/home/jose/.unread_email
-DIR=/home/jose/Mail/unm/Inbox/new
+ENABLE_MBSYNC="$HOME/.enable_mbsync"
+FILE="$HOME/.unread_email"
+DIR="$HOME/Mail/unm/Inbox/new"
 
 while [ 1 ]; do
-  # python ~/.local/scripts/UnseenMail/UnseenMail.py
   if [ "$(cat .enable_mbsync | xargs)" = "yes" ]; then mbsync -a; fi
   echo ' '> $FILE
   ls $DIR | wc -l | xargs >> $FILE
