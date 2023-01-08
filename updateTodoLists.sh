730b480ddf9b54ace4f8464e438a64c0d82a214d
Jose Abel Castellanos Joo <jabelcastellanosjoo@unm.edu>
2023-01-07 23:10:54 -0700

    Update updateotodos script.

--- updateTodoLists.sh
+++ updateTodoLists.sh
@@ -4,7 +4,7 @@ cd $PHD_DOCUMENTS/Org-Files
 git add *.org
 cd $PHD_DOCUMENTS/Seminars/BeihangUniversity-Fall2021/Org-Files
 git add *.org
-cd $PHD_DOCUMENTS/Semesters/Spring/2023/TA-CS-357/Org-Files
+cd $PHD_DOCUMENTS/Semesters//2023/Spring/TA-CS-357/Org-Files
 git add *.org
 cd $PHD_DOCUMENTS/Side-Projects/MaxDiff/Documents/org
 git add *.org
