b38fb5329349deca72e4bff1c0db2f21dbf39800
Jose Abel Castellanos Joo <jabelcastellanosjoo@unm.edu>
2026-04-10 21:45:28 -0600

    Minor changes.

--- typos.sh
+++ typos.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
+FILE_EXT="*.tex"
 DIR="${1:-.}"
 
-find "$DIR" -type f -name "*.tex" -print0 | while IFS= read -r -d '' file; do
+find "$DIR" -type f -name "$FILE_EXT" -print0 | while IFS= read -r -d '' file; do
     if file "$file" | grep -q text; then
         typos=$(aspell list < "$file" | sort -u)
 
